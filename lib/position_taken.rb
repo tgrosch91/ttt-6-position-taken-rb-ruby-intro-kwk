0a77797a5fee4b8acecd3622b310135b0653112e
Teresa Grosch <tgrosch91@gmail.com>
2016-08-20 19:08:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,16 @@
 def position_taken?(board,index)
-  if board[index] == " "||""||nil
-    return false
-  elsif board[index] == "X"||"O"
-    return true
-  else return nil
-  end
+  !(board[index] == " "|| board[index] == "" || board[index]== nil)
 end
 array= [" "," "," "," "," "," "," "," "," "]
 index_number=0
 position_taken?(array,index_number)
-# def position_taken?(board,index)
+# def position_taken?(board,index)def position_taken?(board,index)
+#  if board[index] == " "|| board[index] == "" || board[index]== nil
+#    return false
+#  elsif board[index] == "X"||"O"
+#    return true
+#  else return nil
+#  end
+#end
 #   board[index]!= " "||""||nil
 # end
